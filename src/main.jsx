e8365c7f6702debbcbab88de7713016322f10bee
azzamunza <azzamunza@gmail.com>
2025-10-26 15:39:57 +0800

    Change import from Ecosim.jsx to Ecosim.js

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css'; // import Tailwind-built CSS (PostCSS approach)
-import EcosystemSimulator from './Ecosim.jsx';
+import EcosystemSimulator from './Ecosim.js';
 
 createRoot(document.getElementById('root')).render(<EcosystemSimulator />);
