100c9863a34738e5cddb7adb27e35f41e3a1ed0c
azzamunza <azzamunza@gmail.com>
2025-10-26 10:50:25 +0800

    Import index.css for styling
    Added import statement for index.css to apply styles.

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import './index.css'; // <--- add this line if you created index.css
 import EcosystemSimulator from './Ecosim.jsx';
 
 createRoot(document.getElementById('root')).render(<EcosystemSimulator />);
